0f0112be5268b818c298603a913c8768daaad849
ChikagoDen <perepelkin-da@yandex.ru>
2022-04-19 15:46:30 +0600

    попытка объединить ветки

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfilesController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
