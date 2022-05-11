58630055e178f7078792cc710d765a8b2ac9075f
Yuriy Tigunov <192215@gmail.com>
2022-05-11 17:24:45 +0800

    Fixed css issue (double icons on slider)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,11 @@
 
 @section('content')
 
-    <!-- Carousel wrapper -->
-    <div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
-        <!-- Indicators -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-mdb-ride="carousel">
         <div class="carousel-indicators">
             <button
                 type="button"
-                data-mdb-target="#carouselBasicExample"
+                data-mdb-target="#carouselExampleCaptions"
                 data-mdb-slide-to="0"
                 class="active"
                 aria-current="true"
@@ -16,58 +14,48 @@
             ></button>
             <button
                 type="button"
-                data-mdb-target="#carouselBasicExample"
+                data-mdb-target="#carouselExampleCaptions"
                 data-mdb-slide-to="1"
                 aria-label="Slide 2"
             ></button>
             <button
                 type="button"
-                data-mdb-target="#carouselBasicExample"
+                data-mdb-target="#carouselExampleCaptions"
                 data-mdb-slide-to="2"
                 aria-label="Slide 3"
             ></button>
         </div>
-
-        <!-- Inner -->
         <div class="carousel-inner">
-            <!-- Single item -->
             <div class="carousel-item active">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="Sunset Over the City"/>
+                <img src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" class="d-block w-100" alt="Wild Landscape"/>
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h5>Keep your memories</h5>
+                    <p>Jog someone's memory.</p>
                 </div>
             </div>
-
-            <!-- Single item -->
             <div class="carousel-item">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100" alt="Canyon at Nigh"/>
+                <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" class="d-block w-100" alt="Camera"/>
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h5>Share your creativity</h5>
+                    <p>Get the creative juices flowing.</p>
                 </div>
             </div>
-
-            <!-- Single item -->
             <div class="carousel-item">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100" alt="Cliff Above a Stormy Sea"/>
+                <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" class="d-block w-100" alt="Exotic Fruits"/>
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h5>Let your imagination run riot</h5>
+                    <p>Magic touch.</p>
                 </div>
             </div>
         </div>
-        <!-- Inner -->
-
-        <!-- Controls -->
-        <button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
+        <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleCaptions" data-mdb-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
+        <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleCaptions" data-mdb-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
-    <!-- Carousel wrapper -->
+
 @endsection
