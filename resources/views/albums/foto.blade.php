4892611db6a9da032e0273da1182c993c33a157e
ChikagoDen <perepelkin-da@yandex.ru>
2022-05-18 20:58:39 +0600

    2

--- resources/views/albums/foto.blade.php
+++ resources/views/albums/foto.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div class="foto-form-add-albums">  
             <label class="btn btn2" for="profile-form-hider" id="clickme"><b>Добавить альбом</b></label>
-            {{-- форма редактирования --}}
+            {{-- форма добавить албом --}}
             <div class="profile-form">
                 <input type="checkbox" id="profile-form-hider">
                 <div class="profile-form-edit">
@@ -34,7 +34,6 @@
                                 name="title"
                                 value="{{ old('title') }}"
                                 autocomplete="title">
-
                         </div>
                         @error('title')
                             <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
@@ -58,85 +57,74 @@
                         @error('image')
                             <span class="invalid-feedback"><strong>{{ $message }}</strong></span>
                         @enderror                                
-                        <div class="row mt-5">
+                        <div class="profile-form-div-btn">
                             <button class="btn btn-primary">Создать альбом</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-        <div>
+        <div class="foto-form-add-foto">
             <label class="btn btn2" for="profile-form-foto-hider" id="clickme"><b>Добавить фото</b></label>
-                {{-- форма редактиррования --}}
-                {{-- ТОЖЕ НЕ РАБОТАЕТ --}}
+                {{-- формадобавить фото --}}
+                {{--  НЕ РАБОТАЕТ --}}
                 <div class="profile-form">
                     <input type="checkbox" id="profile-form-foto-hider">
                     <div class="profile-form-edit">
                         <form action={{asset("/a")}} enctype="multipart/form-data" method="post">
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
                             @csrf
-                            <div class="row">
-                                <div class="col-8 offset-2">
-                                    <div class="row mb-3">
-                                        <label for="title" class="col-md-4 col-form-label">Название</label>
-                                        <input id="title"
-                                            type="text"
-                                            class="form-control @error('title') is-invalid @enderror"
-                                            name="title"
-                                            value="{{ old('title') }}"
-                                            autocomplete="title">
-                                                    {{--                        @error('title')--}}
-                                                    {{--                        <span class="invalid-feedback" role="alert">--}}
-                                                    {{--                                        <strong>{{ $message }}</strong>--}}
-                                                    {{--                                    </span>--}}
-                                                    {{--                        @enderror--}}
-                                    </div>
-                                    <div class="row mb-3">
-                                        <label for="description" class="col-md-4 col-form-label">Описание</label>
-                                        <input id="description"
-                                            type="text"
-                                            class="form-control @error('description') is-invalid @enderror"
-                                            name="description"
-                                            value="{{ old('description') }}"
-                                            autocomplete="description">
-                                                        {{--                        @error('description')--}}
-                                                        {{--                        <span class="invalid-feedback" role="alert">--}}
-                                                        {{--                                        <strong>{{ $message }}</strong>--}}
-                                                        {{--                                    </span>--}}
-                                                        {{--                        @enderror--}}
-                                    </div>
-                                    <div class="row mb-3">
-                                        <label for="album" class="col-md-4 col-form-label">Выбор альбома</label>
-                                        <select class="form-select" aria-label="Default select example" name="album">
-                                            @foreach($user->album as $album)
-                                                <option value="{{ $album->id }}"> {{ $album->title }} </option>
-                                            @endforeach
-                                        </select>
-                                                {{--                        @error('album')--}}
-                                                {{--                        <span class="invalid-feedback" role="alert">--}}
-                                                {{--                                        <strong>{{ $message }}</strong>--}}
-                                                {{--                                    </span>--}}
-                                                {{--                        @enderror--}}
-                                    </div>
-                                    <div class="row">
-                                        <label for="image" class="col-md-4 col-form-label">Выбрать фотку</label>
-                                        <input type="file" multiple class="form-control" id="image" name="image[]">
-                                            {{--                        @error('image')--}}
-                                            {{--                                        <strong>{{ $message }}</strong>--}}
-                                            {{--                        @enderror--}}
-                                    </div>
-                                    <div class="row mt-5">
-                                        <button class="btn btn-primary">Загрузить</button>
-                                    </div>
-                                </div>
+                            <div class="profile-form-div">
+                                <label for="title">Название:</label>
+                                <input id="title"
+                                    type="text"
+                                    class="input @error('title') is-invalid @enderror"
+                                    name="title"
+                                    value="{{ old('title') }}"
+                                    autocomplete="title">
+                            </div>
+                            @error('title')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            <div class="profile-form-div">
+                                <label for="description">Описание:</label>
+                                <input id="description"
+                                    type="text"
+                                    class="input @error('description') is-invalid @enderror"
+                                    name="description"
+                                    value="{{ old('description') }}"
+                                    autocomplete="description">
+                            </div>
+                            @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror                                    
+                            <div class="profile-form-div">
+                                <label for="album" >Выбор альбома:</label>
+                                <select class="select" aria-label="Default select example" name="album">
+                                    @foreach($user->album as $album)
+                                        <option value="{{ $album->id }}"> {{ $album->title }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            @error('album')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror 
+                            <div class="profile-form-div foto-add">
+                                <label for="image" class="btn label">Выбрать фото на заставку</label>
+                                <input type="file" id="image" multiple style="display:none;" name="image[]" accept="image/*,image/jpeg">
+                            </div>                                   
+                            @error('image')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>    
+                            @enderror                                    
+                            <div class="profile-form-div-btn">
+                                <button class="btn">Загрузить</button>
                             </div>
                         </form>
                     </div>
