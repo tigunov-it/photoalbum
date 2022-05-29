ef433cc076ad1558a8bf3fa66414ff3c45dce495
Yuriy Tigunov <192215@gmail.com>
2022-05-29 11:28:48 +0800

    1. Add button Go To Profile on the main page for registered user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,9 +19,17 @@
                     <div class="text-white mb-12">
                         <h1 class="mb-3">Keep your memories</h1>
                         <h4 class="mb-3">Jog someone's memory</h4>
-                        <a class="btn btn-outline-light btn-lg" href="{{ env('APP_URL') . '/login' }}" role="button"
-                        >Start for free</a
-                        >
+                        @if(Auth::user())
+                            <a class="btn btn-outline-light btn-lg" href="/profile/{{ Auth::user()->id }}"
+                               role="button"
+                            >Go to My Profile</a
+                            >
+                        @else
+                            <a class="btn btn-outline-light btn-lg" href="{{ env('APP_URL') . '/register' }}"
+                               role="button"
+                            >Start for free</a
+                            >
+                        @endif
                     </div>
 
                 </div>
@@ -38,9 +46,17 @@
                     <div class="text-white mb-12">
                         <h1 class="mb-3">Share your creativity</h1>
                         <h4 class="mb-3">Get the creative juices flowing</h4>
-                        <a class="btn btn-outline-light btn-lg" href="{{ env('APP_URL') . '/login' }}" role="button"
-                        >Start for free</a
-                        >
+                        @if(Auth::user())
+                            <a class="btn btn-outline-light btn-lg" href="/profile/{{ Auth::user()->id }}"
+                               role="button"
+                            >Go to My Profile</a
+                            >
+                        @else
+                            <a class="btn btn-outline-light btn-lg" href="{{ env('APP_URL') . '/register' }}"
+                               role="button"
+                            >Start for free</a
+                            >
+                        @endif
                     </div>
 
                 </div>
@@ -57,16 +73,24 @@
                     <div class="text-white mb-12">
                         <h1 class="mb-3">Let your imagination run riot</h1>
                         <h4 class="mb-3">Magic touch</h4>
-                        <a class="btn btn-outline-light btn-lg" href="{{ env('APP_URL') . '/login' }}" role="button"
-                        >Start for free</a
-                        >
+                        @if(Auth::user())
+                            <a class="btn btn-outline-light btn-lg" href="/profile/{{ Auth::user()->id }}"
+                               role="button"
+                            >Go to My Profile</a
+                            >
+                        @else
+                            <a class="btn btn-outline-light btn-lg" href="{{ env('APP_URL') . '/register' }}"
+                               role="button"
+                            >Start for free</a
+                            >
+                        @endif
                     </div>
 
                 </div>
             </div>
 
-        <!-- Inner -->
+            <!-- Inner -->
 
-    <!-- Carousel wrapper -->
+            <!-- Carousel wrapper -->
 
 @endsection
