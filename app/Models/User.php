997e76241386d792c139951ba59179f498adc09f
grnsv <grin.sv@gmail.com>
2023-03-08 05:20:22 +0300

    Added L5 swagger

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,19 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use OpenApi\Attributes as OAT;
 
+#[OAT\Schema(
+    properties: [
+        new OAT\Property(property: 'id', type: 'integer', format: 'int64', minimum: 1),
+        new OAT\Property(property: 'name', type: 'string', maxLength: 255),
+        new OAT\Property(property: 'email', type: 'string', format: 'email'),
+        new OAT\Property(property: 'username', type: 'string', maxLength: 255),
+        new OAT\Property(property: 'email_verified_at', type: 'string', format: 'date-time', nullable: true),
+        new OAT\Property(property: 'created_at', type: 'string', format: 'date-time', nullable: true),
+        new OAT\Property(property: 'updated_at', type: 'string', format: 'date-time', nullable: true),
+    ],
+)]
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::group(['prefix' => 'v1', 'as' => 'v1.'], function () {
+
+        Route::get('/user', [UserController::class, 'user'])->name('user');
+    });
 });
