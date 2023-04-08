c704941c44913d2af03949235ba81b7ee861eaa2
grnsv <grin.sv@gmail.com>
2023-04-08 03:49:07 +0300

    Added tests

--- tests/Feature/Http/Controllers/V1/AlbumControllerTest.php
+++ tests/Feature/Http/Controllers/V1/AlbumControllerTest.php
@@ -6,14 +6,17 @@ use App\Models\Album;
 use App\Models\User;
 use Aws\Rekognition\RekognitionClient;
 use Aws\Result;
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Mockery;
 use Mockery\MockInterface;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Tests\TestCase;
 
-class AlbumControllerTest extends TestCase
+final class AlbumControllerTest extends TestCase
 {
     public function test_unauthenticated_user_can_not_view_listing_of_albums(): void
     {
@@ -38,12 +41,9 @@ class AlbumControllerTest extends TestCase
         $image = UploadedFile::fake()->image('image.jpg');
         $album = Album::factory()->make(['image' => $image]);
 
-        $this->instance(
-            RekognitionClient::class,
-            Mockery::mock('overload:' . RekognitionClient::class, static function (MockInterface $mock) {
-                $mock->shouldNotReceive('detectModerationLabels');
-            }),
-        );
+        $this->mock('overload:' . RekognitionClient::class, static function (MockInterface $mock): void {
+            $mock->shouldNotReceive('detectModerationLabels');
+        });
 
         $response = $this->post(route('v1.albums.store'), $album->toArray());
 
@@ -56,12 +56,9 @@ class AlbumControllerTest extends TestCase
         $image = UploadedFile::fake()->image('image.jpg');
         $album = Album::factory()->make(['image' => $image]);
 
-        $this->instance(
-            RekognitionClient::class,
-            Mockery::mock('overload:' . RekognitionClient::class, static function (MockInterface $mock) {
-                $mock->shouldReceive('detectModerationLabels')->once()->andReturn(new Result);
-            }),
-        );
+        $this->mock('overload:' . RekognitionClient::class, static function (MockInterface $mock): void {
+            $mock->shouldReceive('detectModerationLabels')->once()->andReturn(new Result);
+        });
 
         $response = $this->actingAs($user)->post(route('v1.albums.store'), $album->toArray());
 
@@ -74,5 +71,63 @@ class AlbumControllerTest extends TestCase
             'description' => $album->description,
             'is_public' => $album->is_public,
         ]);
+    }
+
+    public function test_users_can_not_view_other_users_private_albums(): void
+    {
+        $user1 = User::factory()->create();
+        $album = Album::factory()->for($user1)->create(['is_public' => false]);
+        $user2 = User::factory()->create();
+
+        $response = $this->actingAs($user2)->get(route('v1.albums.show', ['album' => $album->id]));
+
+        $response->assertForbidden();
+    }
+
+    public function test_users_can_view_other_users_public_albums(): void
+    {
+        $user1 = User::factory()->create();
+        $album = Album::factory()->for($user1)->create(['is_public' => true]);
+        $user2 = User::factory()->create();
+
+        $response = $this->actingAs($user2)->get(route('v1.albums.show', ['album' => $album->id]));
+
+        $response->assertOk()->assertJson($album->toArray());
+    }
+
+    public function test_users_can_view_own_private_albums(): void
+    {
+        $user = User::factory()->create();
+        $album = Album::factory()->for($user)->create(['is_public' => false]);
+
+        $response = $this->actingAs($user)->get(route('v1.albums.show', ['album' => $album->id]));
+
+        $response->assertOk()->assertJson($album->toArray());
+    }
+
+    public function test_user_can_view_albums_cover(): void
+    {
+        $user = User::factory()->create();
+        $album = Album::factory()->for($user)->create();
+        $filesystemAdapterMock = $this->mock(FilesystemAdapter::class, static function (MockInterface $mock) use (
+            $album,
+        ): void {
+            $mock->shouldReceive('fileExists')
+                ->once()
+                ->with($album->image)
+                ->andReturn(true);
+            $mock->shouldReceive('download')
+                ->once()
+                ->with($album->image)
+                ->andReturn(new StreamedResponse);
+        });
+        Storage::shouldReceive('disk')
+            ->twice()
+            ->with('s3')
+            ->andReturn($filesystemAdapterMock);
+
+        $response = $this->actingAs($user)->get(route('v1.albums.show.s3cover', ['album' => $album->id]));
+
+        $response->assertOk();
     }
 }
