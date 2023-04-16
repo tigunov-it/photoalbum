c92cade89c399ac2e6ea22c79bf95e1796ed32b3
grnsv <grin.sv@gmail.com>
2023-04-16 15:16:18 +0300

    Updated swagger

--- app/Http/Controllers/Swagger/ApiResponse.php
+++ app/Http/Controllers/Swagger/ApiResponse.php
@@ -6,13 +6,14 @@ use Illuminate\Http\JsonResponse;
 use OpenApi\Attributes as OAT;
 
 #[
+    OAT\Schema(schema: 'ValidationErrors', required: ['message', 'errors'], properties: [
+        new OAT\Property(property: 'message', type: 'string'),
+        new OAT\Property(property: 'errors', type: 'object', example: ['field' => ['message']]),
+    ]),
     OAT\Response(
         response: 'UnprocessableEntity',
         description: 'Validation errors',
-        content: new OAT\JsonContent(required: ['message', 'errors'], properties: [
-            new OAT\Property(property: 'message', type: 'string'),
-            new OAT\Property(property: 'errors', type: 'object'),
-        ]),
+        content: new OAT\JsonContent(ref: '#/components/schemas/ValidationErrors'),
     ),
 ] final class ApiResponse
 {
