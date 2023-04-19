076241aafc558fadbcaf090b8d234b780a35b5f4
grnsv <grin.sv@gmail.com>
2023-04-19 23:37:02 +0300

    Updated rector config

--- rector.php
+++ rector.php
@@ -2,26 +2,26 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    // $rectorConfig->paths([
-    //     __DIR__ . '/app',
-    //     __DIR__ . '/bootstrap',
-    //     __DIR__ . '/config',
-    //     __DIR__ . '/lang',
-    //     __DIR__ . '/public',
-    //     __DIR__ . '/resources',
-    //     __DIR__ . '/routes',
-    //     __DIR__ . '/tests',
-    // ]);
+    $rectorConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/resources/views',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ]);
 
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->parallel(seconds: 360);
+
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
 
     // define sets of rules
     $rectorConfig->sets([
@@ -44,5 +44,12 @@ return static function (RectorConfig $rectorConfig): void {
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
         LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        PHPUnitSetList::PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::PHPUNIT_EXCEPTION,
+        PHPUnitSetList::REMOVE_MOCKS,
+        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
+        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
+        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
     ]);
 };
