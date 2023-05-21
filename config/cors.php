05702653972615d3f42b18f19af1bafefb1fa70a
grnsv <grin.sv@gmail.com>
2023-05-21 23:15:46 +0300

    CORS test

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'http://localhost',
+    ],
 
     'allowed_origins_patterns' => [],
 
