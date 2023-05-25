9027725c489b4108e33560d6fea5bb7159668ec8
grnsv <grin.sv@gmail.com>
2023-05-26 01:31:30 +0300

    CORS test

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class HandleCors extends \Illuminate\Http\Middleware\HandleCors
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$this->hasMatchingPath($request)) {
+            return $next($request);
+        }
+
+        $options = $this->container['config']->get('cors', []);
+        Log::debug('options', $options);
+
+        $this->cors->setOptions($options);
+
+        if ($this->cors->isPreflightRequest($request)) {
+            $response = $this->cors->handlePreflightRequest($request);
+
+            $this->cors->varyHeader($response, 'Access-Control-Request-Method');
+
+            Log::debug('preflight', $response->headers->all());
+
+            return $response;
+        }
+
+        $response = $next($request);
+
+        if ($request->getMethod() === 'OPTIONS') {
+            $this->cors->varyHeader($response, 'Access-Control-Request-Method');
+        }
+
+        $response = $this->cors->addActualRequestHeaders($response, $request);
+
+        Log::debug('headers', $response->headers->all());
+
+        return $response;
+    }
+}
