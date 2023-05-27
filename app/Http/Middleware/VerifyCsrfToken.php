51906f317bf2766e88ea2c6a7335e35575397f40
grnsv <grin.sv@gmail.com>
2023-05-27 20:38:45 +0300

    debug XSRF cookie

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,46 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Illuminate\Session\TokenMismatchException
+     */
+    public function handle($request, \Closure $next)
+    {
+        $isReading = $this->isReading($request);
+        $runningUnitTests = $this->runningUnitTests();
+        $inExceptArray = $this->inExceptArray($request);
+        $tokensMatch = $this->tokensMatch($request);
+        $shouldAddXsrfTokenCookie = $this->shouldAddXsrfTokenCookie();
+
+        \Illuminate\Support\Facades\Log::channel('telegram')->debug(
+            $request->fullUrl(),
+            compact('isReading', 'runningUnitTests', 'inExceptArray', 'tokensMatch', 'shouldAddXsrfTokenCookie'),
+        );
+
+        if (
+            $isReading ||
+            $runningUnitTests ||
+            $inExceptArray ||
+            $tokensMatch
+        ) {
+            return tap($next($request), function ($response) use ($request) {
+                if ($this->shouldAddXsrfTokenCookie()) {
+
+                    \Illuminate\Support\Facades\Log::channel('telegram')->debug(
+                        $this->newCookie($request, config('session')),
+                    );
+                    $this->addCookieToResponse($request, $response);
+                }
+            });
+        }
+
+        throw new \Illuminate\Session\TokenMismatchException('CSRF token mismatch.');
+    }
 }
