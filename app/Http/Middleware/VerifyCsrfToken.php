41599ef5cdcd235581b869a3e07fc703bda3ad68
grnsv <grin.sv@gmail.com>
2023-05-28 00:33:59 +0300

    test XSRF cookie

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,37 +14,4 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     *
-     * @throws \Illuminate\Session\TokenMismatchException
-     */
-    public function handle($request, \Closure $next)
-    {
-        \Illuminate\Support\Facades\Log::channel('telegram')->debug($request->fullUrl());
-
-        if (
-            $this->isReading($request) ||
-            $this->runningUnitTests() ||
-            $this->inExceptArray($request) ||
-            $this->tokensMatch($request)
-        ) {
-            return tap($next($request), function ($response) use ($request) {
-                \Illuminate\Support\Facades\Log::channel('telegram')->debug((string) $this->shouldAddXsrfTokenCookie());
-                if ($this->shouldAddXsrfTokenCookie()) {
-                    \Illuminate\Support\Facades\Log::channel('telegram')->debug(
-                        $this->newCookie($request, config('session')),
-                    );
-                    $this->addCookieToResponse($request, $response);
-                }
-            });
-        }
-
-        throw new \Illuminate\Session\TokenMismatchException('CSRF token mismatch.');
-    }
 }
