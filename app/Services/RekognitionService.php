fefa2b78304dc2a4e75b2ba568227a05d55839bb
Yuriy Tigunov <192215@gmail.com>
2023-06-01 18:35:13 +0800

    Fix RekognitionService

--- app/Services/RekognitionService.php
+++ app/Services/RekognitionService.php
@@ -5,7 +5,7 @@ namespace App\Services;
 use Aws\Rekognition\RekognitionClient;
 use Illuminate\Http\UploadedFile;
 
-final readonly class RekognitionService
+final class RekognitionService
 {
     private RekognitionClient $client;
 
